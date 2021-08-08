64c9ac31659d24393026018e55910f8e8e4a5f4b
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2021-08-08 22:25:37 +0300

    Add files via upload

---
+++ QR Code APP FORM/QR Code APP FORM/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessagingToolkit.QRCode.Codec;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace QR_Code_APP_FORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnencode_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder encoder = new QRCodeEncoder();
+
+            pictureBox1.Image = encoder.Encode(txtboxinput.Text);
+        }
+
+        private void btndecode_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder decoder = new QRCodeDecoder();
+
+            txtboxinput.Text = decoder.Decode(new QRCodeBitmapImage(pictureBox1.Image as Bitmap));
+        }
+    }
+}
