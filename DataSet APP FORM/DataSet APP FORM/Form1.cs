43c0da30053ddd6e3d2453975c7620b84b1addf6
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2021-08-08 22:06:56 +0300

    Add files via upload

---
+++ DataSet APP FORM/DataSet APP FORM/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSet_APP_FORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       DataSet1TableAdapters.ÖrnekKitapListesiTableAdapter adapter= new DataSet1TableAdapters.ÖrnekKitapListesiTableAdapter();
+
+        DataSet1.ÖrnekKitapListesiDataTable dt = new DataSet1.ÖrnekKitapListesiDataTable();
+
+        private void showdata()
+        {
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showdata();
+        }
+    }
+}
---
+++ Delegate App Form/Delegate App Form/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delegate_App_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        delegate void islem(int x1,int x2);
+
+        void topla(int a1, int a2)
+        {
+            listBox1.Items.Add(a1+a2);
+        }
+
+        void cıkar(int b1,int b2)
+        {
+            listBox1.Items.Add(b1-b2);
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            islem i = new islem(topla);
+            i += cıkar;
+
+            i(10, 5);
+
+
+            
+
+           
+        }
+    }
+}
