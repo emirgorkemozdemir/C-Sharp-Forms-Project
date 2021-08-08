81a5d6d1327d6fdae2a993d3334bc46c0df90bd6
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2021-08-08 22:28:12 +0300

    Add files via upload

---
+++ RSS APP FORM/RSS APP FORM/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace RSS_APP_FORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbox = new System.Windows.Forms.ListBox();
+            this.btnshow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbox
+            // 
+            this.lbox.FormattingEnabled = true;
+            this.lbox.Location = new System.Drawing.Point(12, 12);
+            this.lbox.Name = "lbox";
+            this.lbox.Size = new System.Drawing.Size(522, 251);
+            this.lbox.TabIndex = 0;
+            // 
+            // btnshow
+            // 
+            this.btnshow.Location = new System.Drawing.Point(179, 281);
+            this.btnshow.Name = "btnshow";
+            this.btnshow.Size = new System.Drawing.Size(177, 89);
+            this.btnshow.TabIndex = 1;
+            this.btnshow.Text = "Haberleri Göster";
+            this.btnshow.UseVisualStyleBackColor = true;
+            this.btnshow.Click += new System.EventHandler(this.btnshow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 382);
+            this.Controls.Add(this.btnshow);
+            this.Controls.Add(this.lbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbox;
+        private System.Windows.Forms.Button btnshow;
+    }
+}
+
---
+++ RSS APP FORM/RSS APP FORM/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace RSS_APP_FORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnshow_Click(object sender, EventArgs e)
+        {
+            XmlTextReader xmloku = new XmlTextReader("https://www.cnnturk.com/feed/rss/all/news");
+
+            while (xmloku.Read())
+            {
+                if (xmloku.Name=="description")
+                {
+                    lbox.Items.Add(xmloku.ReadString());
+                }
+            }
+
+        }
+    }
+}
