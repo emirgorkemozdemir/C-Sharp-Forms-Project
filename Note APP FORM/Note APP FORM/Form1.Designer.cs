c5c6ae2891bb878b228fa00e51a6b645acd8a2fa
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2021-08-08 22:22:09 +0300

    Add files via upload

---
+++ Note APP FORM/Note APP FORM/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Note_APP_FORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.dp1 = new System.Windows.Forms.DateTimePicker();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.txtboxnote = new System.Windows.Forms.RichTextBox();
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // dp1
+            // 
+            this.dp1.CalendarFont = new System.Drawing.Font("Bahnschrift SemiCondensed", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dp1.Location = new System.Drawing.Point(12, 12);
+            this.dp1.Name = "dp1";
+            this.dp1.Size = new System.Drawing.Size(200, 20);
+            this.dp1.TabIndex = 0;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsave.Location = new System.Drawing.Point(12, 38);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(200, 55);
+            this.btnsave.TabIndex = 1;
+            this.btnsave.Text = "Notu Kaydet";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // txtboxnote
+            // 
+            this.txtboxnote.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtboxnote.Location = new System.Drawing.Point(218, 12);
+            this.txtboxnote.Name = "txtboxnote";
+            this.txtboxnote.Size = new System.Drawing.Size(337, 81);
+            this.txtboxnote.TabIndex = 2;
+            this.txtboxnote.Text = "";
+            // 
+            // dg1
+            // 
+            this.dg1.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dg1.Location = new System.Drawing.Point(12, 108);
+            this.dg1.Name = "dg1";
+            this.dg1.Size = new System.Drawing.Size(543, 208);
+            this.dg1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(567, 328);
+            this.Controls.Add(this.dg1);
+            this.Controls.Add(this.txtboxnote);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.dp1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.DateTimePicker dp1;
+        private System.Windows.Forms.RichTextBox txtboxnote;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.DataGridView dg1;
+    }
+}
+
---
+++ Note APP FORM/Note APP FORM/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Note_APP_FORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connect = new SqlConnection("Data Source=.;Initial Catalog=TorosDTB;Integrated Security=True");
+
+        private void showdata()
+        {
+            connect.Open();
+            SqlCommand commandshow = new SqlCommand("Select *from TabloNotDefteri", connect);
+            SqlDataAdapter da = new SqlDataAdapter(commandshow);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dg1.DataSource = dt.DefaultView;
+            connect.Close();
+        }
+
+        private void savedata(string temptext)
+        {
+            connect.Open();
+            SqlCommand commandsave = new SqlCommand("Insert into TabloNotDefteri (Note) values (@notu)",connect);
+            commandsave.Parameters.AddWithValue("@notu", temptext);
+            commandsave.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            
+            string text = txtboxnote.Text;
+            string date = dp1.Value.ToString();
+            string text2 = $"{text} {date}";
+            savedata(text2);
+            showdata();
+        }
+    }
+}
